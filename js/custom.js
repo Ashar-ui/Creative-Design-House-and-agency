908c9ada8919fd8cdffab63709bf250dcc2978bc
ashar <asharhashmi990@gmail.com>
2024-01-15 15:58:28 -0500

    first commit

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+var acc = document.getElementsByClassName("help");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+$(".menu").click(function() {
+    $("span").toggleClass("change");
+    $("ul").slideToggle();
+});
